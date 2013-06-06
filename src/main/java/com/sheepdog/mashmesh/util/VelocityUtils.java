16cc8b2ebd40778e4e2f9928dcb96c74142febeb
Brendan MacDonell <macdonellba@gmail.com>
2013-06-06 11:54:01 -0300

    Add escape utils to velocity

--- src/main/java/com/sheepdog/mashmesh/util/VelocityUtils.java
+++ src/main/java/com/sheepdog/mashmesh/util/VelocityUtils.java
@@ -1,14 +1,17 @@
 package com.sheepdog.mashmesh.util;
 
 import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.context.Context;
+import org.apache.velocity.tools.ToolManager;
 
 import java.io.StringWriter;
 import java.util.Properties;
 
 public class VelocityUtils {
     private static VelocityEngine engine = null;
+    private static ToolManager toolManager = null;
 
     public static VelocityEngine getInstance() {
         if (engine == null) {
@@ -22,6 +25,16 @@ public class VelocityUtils {
         }
 
         return engine;
+    }
+
+    public static VelocityContext getContext() {
+        if (toolManager == null) {
+            ToolManager newToolManager = new ToolManager();
+            newToolManager.configure("WEB-INF/velocity-tools.xml");
+            toolManager = newToolManager;
+        }
+
+        return new VelocityContext(toolManager.createContext());
     }
 
     public static String renderTemplateToString(String templatePath, Context context) {
